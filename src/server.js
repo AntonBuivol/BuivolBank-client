330d592cb691e0fe799df8075400754294aa44c8
AntonBuivol <anton.buivol@gmail.com>
2024-10-23 19:59:41 +0300

    As a user I can register [Delivers #188295966]

--- src/server.js
+++ src/server.js
@@ -2,17 +2,35 @@ import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
 import * as sapper from '@sapper/server';
-const { json } = require('body-parser');
+import session from 'express-session';
+import sessionFileStore from 'session-file-store'; // npm install session-file-store
 
+const { json } = require('body-parser');
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
-
+const FileStore = new sessionFileStore(session);// faili salvestamine
 polka() // You can also use Express
 	.use(
 		compression({ threshold: 0 }),
 		json(),
+		session({
+			secret: 'bvl', //suvaline EI tohi githubi panna
+			resave: true,
+			saveUninitialized: true,
+			cookie: {
+				maxAge: 31536000
+			},
+			store: new FileStore({
+				path: `.sessions`
+			})
+		}),
 		sirv('static', { dev }),
-		sapper.middleware()
+		sapper.middleware({
+			session: req => ({
+				user: req.session && req.session.user,
+				token: req.session && req.session.token
+			})
+		})
 	)
 	.listen(PORT, err => {
 		if (err) console.log('error', err);
